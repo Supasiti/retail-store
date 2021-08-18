c49b7327f1aba9a69bf3407be89074f53c363396
Thara Supasiti <sapiensis@gmail.com>
2021-08-18 18:10:23 +1000

    connect get /api/categories/ to category.getAll

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,11 +1,18 @@
 const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const category = require('../../services/categoryServices')
+const sanitize = require('../../services/sanitize')
 
 // The `/api/categories` endpoint
 
+// find all categories
 router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+  try {
+    const categoriesData = await category.getAll();
+    const sanitized = sanitize(categoriesData);
+    res.status(200).json(sanitized);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,12 +1,11 @@
 const router = require('express').Router();
-const { Product, Category, Tag, ProductTag } = require('../../models');
 const product = require('../../services/productServices')
 const sanitize = require('../../services/sanitize')
 
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   try {
     const productsData = await product.getAll();
     const sanitized = sanitize(productsData);
@@ -17,7 +16,7 @@ router.get('/', (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   try {
     const productData = await product.getOne(req.params.id);
     const sanitized = sanitize(productData);
@@ -68,7 +67,7 @@ router.post('/', async (req, res) => {
 });
 
 // update product
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
 
   try {
     const updatedProductTags = await product.update(req.body, req.params.id);
@@ -118,7 +117,7 @@ router.put('/:id', (req, res) => {
   //   });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
   try {
     const idsRemoved = await product.remove(req.params.id);
